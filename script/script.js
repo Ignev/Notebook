9923127cfe12a42a3a9bd4a70289425a05bc00c6
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2020-07-02 16:51:19 +0300

    Add function editing Note

--- script/script.js
+++ script/script.js
@@ -1,68 +1,98 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", () => {
+    
+    let notes = [],
+        editing = false,
+        curNum,
+        curNote;
     const noteText = document.querySelector(".main_textarea"),
           noteName = document.querySelector(".main_note-title"),
           btnSave  = document.querySelector(".main_btn"),
           list     = document.querySelector(".aside-right_list"),
-          li       = document.querySelectorAll(".list_item");
-    let notes = [];
-    const addZero = (num) => {
+    addZero = (num) => {
         if(num > 0 && num < 9){
-            return num + "0";
+            return "0" + num ;
         }
-    };
-    const addData = () => {
-        btnSave.addEventListener('click', () =>{
-            let date = new Date;
-            let now = 
-                addZero(date.getHours()) + ':' +
-                addZero(date.getMinutes()) + ':' +
-                addZero(date.getSeconds()) + ' ' +
-                addZero(date.getDate()) + '.' +
-                addZero(date.getMonth() + 1) + '.' +
-                date.getFullYear();
+    }, //Добавление нуля к часам, минутам, секундам, если они > 9
+    recordSelection = () => {
+        list.addEventListener('click', event => {
+            let num = event.target.dataset.num;
+            noteText.value = notes[num].text;
+            noteName.value = notes[num].name;
+            curNum = num;
+            curNote = event.target;
+            console.log(curNum);
+            console.log(curNote);
+
+            
+            editing = true;
+        });
+    },
+    editNote = () => {
+        notes[curNum].text = noteText.value;
+        notes[curNum].name = noteName.value;
+        if(noteName.value === " "){
+            curNote.innerHTML = "No name";
+        }
+        else{
+            curNote.innerHTML = noteName.value;
+        }
+        noteText.value = " ";
+        noteName.value = " ";
+        curNum = '';
+        curNote = '';
+    },
+    addData = () => {
+            let date = new Date,
+            now = 
+            date.getHours() + ':' +
+            date.getMinutes() + ':' +
+            date.getSeconds() + ' ' +
+            addZero(date.getDate()) + '.' +
+            addZero(date.getMonth() + 1) + '.' +
+            date.getFullYear();
             notes.push({text: noteText.value, name: noteName.value, time: now});
-            const listItem = document.createElement('li')
-            listItem.innerHTML = noteName.value;
+            const listItem   = document.createElement('li'),
+                buttonItem = document.createElement('button');
+            if(noteName.value === " "){
+                buttonItem.innerHTML = "No name";
+            }
+            else{
+                buttonItem.innerHTML = noteName.value;
+            }
+            buttonItem.classList.add('list_button');
+            buttonItem.dataset.num = notes.length - 1;
+            listItem.appendChild(buttonItem);
             listItem.classList.add('list_item');
-            listItem.dataset.num = notes.length - 1;
             list.appendChild(listItem);
             noteText.value = " ";
             noteName.value = " ";
+    }, // Функия занесения записей в массив
+    trackingSave = () => {
+        btnSave.addEventListener('click', () =>{
+            if(editing){
+                editNote()
+            }
+            else{
+                addData();
+            }
+            editing = false;
         });
         document.addEventListener('keydown', event => {
             if(event.code == 'KeyS' && (event.ctrlKey || event.metaKey)){
-                let date = new Date;
-            let now = 
-                addZero(date.getHours()) + ':' +
-                addZero(date.getMinutes()) + ':' +
-                addZero(date.getSeconds()) + ' ' +
-                addZero(date.getDate()) + '.' +
-                addZero(date.getMonth() + 1) + '.' +
-                date.getFullYear();
-            notes.push({text: noteText.value, name: noteName.value, time: now});
-            const listItem = document.createElement('li')
-            listItem.innerHTML = noteName.value;
-            listItem.classList.add('list_item');
-            listItem.dataset.num = notes.length - 1;
-            list.appendChild(listItem);
-            noteText.value = " ";
-            noteName.value = " ";
+                event.preventDefault();
+                if(editing){
+                    editNote();
+                }
+                else{
+                    addData();
+                }
+                editing = false;
             }
         });
-    };
-    const editNote = () => {
-        console.log(li);
-        li.forEach(el => {
-            el.addEventListener('click', ()=>{
-                console.log(el);
-                let num = this.dataset.num;
-                noteText.value = notes[num].text;
-                console.log(1);
-            });
-        });
-    };
-    addData();
-    editNote();
+    }; // Сохранение записей
+    // console.log(curNote);
+    trackingSave();
+    recordSelection();
 });
